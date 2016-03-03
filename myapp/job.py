cc0d80d59e9dc9ca72b7e7d41a7d7493a739f294
Hussain Tamboli <hussain@screen-magic.com>
2016-03-03 18:21:14 +0530

    retried the /auth. Didn't work. So reverted changes

---
+++ myapp/job.py
@@ -0,0 +1,21 @@
+__author__ = 'hussain'
+
+
+class Job:
+
+    """
+    @api {get} /job/:id Get Job Details
+    @apiVersion 0.1.0
+    @apiName jobDetails
+    @apiGroup Job
+
+    @apiParam {String} id Job Id
+
+    @apiSuccess {String} title Job title
+
+    @apiHeader {String} Authorization JWT
+    """
+
+    def get(self):
+        pass
+
